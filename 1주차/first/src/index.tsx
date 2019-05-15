28e5552f0b30adc9a3026dc37d6ffa03ff44cc87
홍성준 <hackish@hanmail.net>
2019-05-15 19:17:02 +0900

    우아한 테크러닝 1일차 내용수정

--- 1주차/first/src/index.tsx
+++ 1주차/first/src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement: HTMLElement = document.getElementById("root");
+
+render(<App />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
