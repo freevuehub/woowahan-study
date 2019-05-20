121e95fad4208d3270c006ab95177d17f388900e
홍성준 <hackish@hanmail.net>
2019-05-20 15:18:01 +0900

    우아한 테크러닝 1주

--- first_week/src/index.tsx
+++ first_week/src/index.tsx
@@ -3,11 +3,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { StoreState } from './types';
-import reducer from './reducer';
+import reducer from './reducers';
 import App from './App';
 
 const store: StoreState = createStore(reducer);
-const rootElement = HTMLElement = document.getElementById('root');
+const rootElement: HTMLElement = document.getElementById('root');
 
 render(
   <Provider store={ store }>
